15605ea976ba2a22cfe53ce7d697c2aa3d30f818
Will Lytton <will@Your-worst-nightmare.local>
2015-11-15 21:28:55 +0000

    Updated navigation

--- public/modules/sideNav/components/navigation.jsx
+++ public/modules/sideNav/components/navigation.jsx
@@ -7,17 +7,27 @@ var Navigation = React.createClass({
         console.log("This is firing");
     },
     
+    userProfileClicked: function() {
+        
+    },
+    
     render: function() {
         return (
             <div className="navigation-inner">
                 
                 <div className="navigation-user">
                     <div className="navigation-profile">
-                        <div className="navigation-avatar">
+                        
+                        <div className="navigation-profile-avatar">
+                            <img src="#" alt="" />
                         </div>
                         
-                        <a className="navigation-name" href="#" onClick={this.userNameClicked}>
+                        <a className="navigation-profile-name" href="#" onClick={this.userNameClicked}>
                             <h2>Will Lytton</h2>
+                        </a>
+                        
+                        <a className="navigation-profile-edit" href="#" onClick={this.userProfileClicked}>
+                            Edit
                         </a>
                     </div>
                 </div>
