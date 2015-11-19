81524163146df18b5da36dc91846e3479f9e7587
Will Lytton <will.lytton@gmail.com>
2015-11-19 22:05:00 +0000

    Added dashboard foundation

--- public/core/js/app.js
+++ public/core/js/app.js
@@ -1,11 +1,13 @@
 var Willbatross = require('willbatross');
 var Router = require('router');
 var Navigation = require('modules/sideNav/js/navigationController');
+var Dashboard = require("modules/dashboard/js/dashboardController");
 
 Willbatross.router = new Router();
 
 $(function(){
     
     new Navigation();
-    
+    new DasbBoard();
+
 })
---
+++ public/modules/dashboard/components/dashboard.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var THREE = require('core/js/libraries/three.min.js');
+
+var Dashboard = React.createClass({
+    
+    render: function() {
+        console.log(THREE);
+        
+        return (
+            <div>
+            </div>
+        )
+    }
+    
+});
+
+module.exports = Dashboard;
